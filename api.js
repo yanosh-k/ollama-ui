3fd914a5bcf9cdfe470e6fcc5dffedcad17ba447
Yanosh Kunsh <me@yanosh.net>
2024-01-15 18:28:44 +0200

    add options for basic auth; add options for import export; fix display for mobile devices

--- api.js
+++ api.js
@@ -1,97 +1,118 @@
 var rebuildRules = undefined;
 if (typeof chrome !== "undefined" && chrome.runtime && chrome.runtime.id) {
     rebuildRules = async function (domain) {
-    const domains = [domain];
-    /** @type {chrome.declarativeNetRequest.Rule[]} */
-    const rules = [{
-      id: 1,
-      condition: {
-        requestDomains: domains
-      },
-      action: {
-        type: 'modifyHeaders',
-        requestHeaders: [{
-          header: 'origin',
-          operation: 'set',
-          value: `http://${domain}`,
-        }],
-      },
-    }];
-    await chrome.declarativeNetRequest.updateDynamicRules({
-      removeRuleIds: rules.map(r => r.id),
-      addRules: rules,
-    });
-  }
+        const domains = [domain];
+        /** @type {chrome.declarativeNetRequest.Rule[]} */
+        const rules = [{
+                id: 1,
+                condition: {
+                    requestDomains: domains
+                },
+                action: {
+                    type: 'modifyHeaders',
+                    requestHeaders: [{
+                            header: 'origin',
+                            operation: 'set',
+                            value: `http://${domain}`
+                        }]
+                }
+            }];
+        await chrome.declarativeNetRequest.updateDynamicRules({
+            removeRuleIds: rules.map(r => r.id),
+            addRules: rules
+        });
+    };
 }
 
 
 var ollama_host = localStorage.getItem("host-address");
-if (!ollama_host){
-  ollama_host = 'http://localhost:11434'
+if (!ollama_host) {
+    ollama_host = 'http://localhost:11434';
 } else {
-  document.getElementById("host-address").value = ollama_host;
+    document.getElementById("host-address").value = ollama_host;
 }
 
-if (rebuildRules){
-  rebuildRules(ollama_host);
-}
 
-function setHostAddress(){
-  ollama_host = document.getElementById("host-address").value;
-  localStorage.setItem("host-address", ollama_host);
-  populateModels();
-  if (rebuildRules){
+if (rebuildRules) {
     rebuildRules(ollama_host);
-  }
+}
+
+function setHostAddress() {
+    ollama_host = document.getElementById("host-address").value;
+    localStorage.setItem("host-address", ollama_host);
+    populateModels();
+    if (rebuildRules) {
+        rebuildRules(ollama_host);
+    }
 }
 
 
 
-async function getModels(){
-  const response = await fetch(`${ollama_host}/api/tags`);
-  const data = await response.json();
-  return data;
+async function getModels() {
+    var headers = {};
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username) {
+        headers['Authorization'] = 'Basic ' + btoa(username + ":" + password);
+    }
+
+    const response = await fetch(`${ollama_host}/api/tags`, {
+        headers: headers
+    });
+    const data = await response.json();
+    return data;
 }
 
 
 // Function to send a POST request to the API
 function postRequest(data, signal) {
-  const URL = `${ollama_host}/api/generate`;
-  return fetch(URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data),
-    signal: signal
-  });
+    const URL = `${ollama_host}/api/generate`;
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var headers = {
+        'Content-Type': 'application/json'
+    };
+
+    if (username) {
+        headers['Authorization'] = 'Basic ' + btoa(username + ":" + password);
+    }
+
+
+    return fetch(URL, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(data),
+        signal: signal
+    });
 }
 
 // Function to stream the response from the server
 async function getResponse(response, callback) {
-  const reader = response.body.getReader();
-  let partialLine = '';
+    const reader = response.body.getReader();
+    let partialLine = '';
 
-  while (true) {
-    const { done, value } = await reader.read();
-    if (done) {
-      break;
+    while (true) {
+        const {done, value} = await reader.read();
+        if (done) {
+            break;
+        }
+        // Decode the received value and split by lines
+        const textChunk = new TextDecoder().decode(value);
+        const lines = (partialLine + textChunk).split('\n');
+        partialLine = lines.pop(); // The last line might be incomplete
+
+        for (const line of lines) {
+            if (line.trim() === '')
+                continue;
+            const parsedResponse = JSON.parse(line);
+            callback(parsedResponse); // Process each response word
+        }
     }
-    // Decode the received value and split by lines
-    const textChunk = new TextDecoder().decode(value);
-    const lines = (partialLine + textChunk).split('\n');
-    partialLine = lines.pop(); // The last line might be incomplete
 
-    for (const line of lines) {
-      if (line.trim() === '') continue;
-      const parsedResponse = JSON.parse(line);
-      callback(parsedResponse); // Process each response word
+    // Handle any remaining line
+    if (partialLine.trim() !== '') {
+        const parsedResponse = JSON.parse(partialLine);
+        callback(parsedResponse);
     }
-  }
-
-  // Handle any remaining line
-  if (partialLine.trim() !== '') {
-    const parsedResponse = JSON.parse(partialLine);
-    callback(parsedResponse);
-  }
 }
